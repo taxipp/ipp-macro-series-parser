f25ecefee7dc12a3dbb12e58c1d63d45050735b6
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-07-21 16:49:49 +0200

    New function

--- ipp_macro_series_parser/agregats_transports/transports_tidy_data.py
+++ ipp_macro_series_parser/agregats_transports/transports_tidy_data.py
@@ -16,7 +16,17 @@ def tidy_melt_categorie_index(data_frame):
         value_name = 'unite', var_name = 'annee')
     return data_frame
 
-g_3a = tidy_melt_categorie_index(g_3a)
-g_3a.rename(columns = {'unite': 'conso_millier_m3'}, inplace = True)
-g2_1 = tidy_melt_categorie_index(g2_1)
-g2_1.rename(columns = {'unite': 'parc_millier_veh'}, inplace = True)
+# g_3a = tidy_melt_categorie_index(g_3a)
+# g_3a.rename(columns = {'unite': 'conso_millier_m3'}, inplace = True)
+# g2_1 = tidy_melt_categorie_index(g2_1)
+# g2_1.rename(columns = {'unite': 'parc_millier_veh'}, inplace = True)
+
+
+def tidy_column_names(data_frame):
+    data_frame.columns = data_frame.columns.astype(str)
+    for year in range[1990, 2014]:
+        data_frame.rename(columns = {'{}'.format(year): '_{}'.format(year)}, inplace = True)
+    return data_frame
+
+g_3a = tidy_column_names(g_3a)
+g2_1 = tidy_column_names(g2_1)
