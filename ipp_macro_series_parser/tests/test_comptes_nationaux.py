5d558ccdb585e9d70afb8365ef9cc178b944d7ac
antoinearnoud <antoine.arnoud@gmail.com>
2015-07-28 16:58:15 +0200

    more tests : duplicates in non tee and duplicates in final dataframe

--- ipp_macro_series_parser/tests/test_comptes_nationaux.py
+++ ipp_macro_series_parser/tests/test_comptes_nationaux.py
@@ -2,6 +2,7 @@
 
 
 from ipp_macro_series_parser.comptes_nationaux import cn_parser_tee
+from ipp_macro_series_parser.comptes_nationaux import cn_parser_non_tee
 from ipp_macro_series_parser.comptes_nationaux import cn_parser_main
 
 
@@ -13,6 +14,14 @@ def test_duplicate_tee_df():
             assert element == 0, "There are duplicate rows in TEE " + key + ", in folder: comptes_annees " + folder_year
 
 
+def test_duplicate_non_tee_df():
+    folder_year = 2013
+    non_tee_df_by_filename = cn_parser_non_tee.non_tee_df_by_filename_generator(folder_year)
+    for key, df in non_tee_df_by_filename.items():
+        for element in df.duplicated():
+            assert element == 0, "There are duplicate rows in " + key + ", in folder: comptes_annees " + folder_year
+
+
 def test_cn_main1():
     try:
         cn_parser_main.cn_df_generator(2013)
@@ -20,3 +29,9 @@ def test_cn_main1():
     except:
         result = False
     assert result, "The final table of comptabilite nationale could not be generated in cn_main"
+
+
+def test_cn_main2():
+    df = cn_parser_main.cn_df_generator(2013)
+    for element in df.duplicated():
+        assert element == 0, "The final table of comptabilite nationale contains duplicates"
