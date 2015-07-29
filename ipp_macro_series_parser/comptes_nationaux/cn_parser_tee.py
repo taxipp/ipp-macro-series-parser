d2bc32642401985056e90c1c24829cbefb8083bd
SophieIPP <cottet.s@gmail.com>
2015-07-29 12:24:19 +0200

    Fix big bug which inverted Ressources and Emplois

--- ipp_macro_series_parser/comptes_nationaux/cn_parser_tee.py
+++ ipp_macro_series_parser/comptes_nationaux/cn_parser_tee.py
@@ -58,8 +58,8 @@ def tee_file_parser(excelfile_name):
     df_rp = pandas.read_excel(excelfile_name, sheetname = 2, header = header, skiprows = skiprows,
                               skip_footer = skip_footer, index_col = index_col, parse_cols = parse_cols,
                               names = col_names_passifs)
-    df_ea['ressources'] = True
-    df_rp['ressources'] = False
+    df_ea['ressources'] = False
+    df_rp['ressources'] = True
 
     for df in [df_ea, df_rp]:
         df['year'] = int(infos['year'])
