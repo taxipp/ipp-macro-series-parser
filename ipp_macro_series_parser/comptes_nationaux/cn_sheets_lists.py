ba4bfdc5530a298bb47b66f03dd61045cf80f178
SophieIPP <cottet.s@gmail.com>
2015-07-30 12:32:53 +0200

    Rearranges files : - function output_for_sheets in cn_output - a script with only the (input) lists of variables to parse to recreate an excel sheet - application of this function is put in cn_test
    Creates docstring for output_for_sheets

--- ipp_macro_series_parser/comptes_nationaux/cn_sheets_lists.py
+++ ipp_macro_series_parser/comptes_nationaux/cn_sheets_lists.py
@@ -4,37 +4,13 @@ Created on Mon Jul 27 10:22:19 2015
 
 @author: sophie.cottet
 
-This script recreates the different excel sheets of Agrégats IPP - Comptabilité nationale.xls
-i.e. an economic / Piketty presentation of the Comptabilité nationale agregates.
+This script gives the list dictionnaries of the variables needed to recreate the different excel sheets of
+'Agrégats IPP - Comptabilité nationale.xls', i.e. the economic / Piketty presentation of the Comptabilité nationale
+agregates.
+These lists are called by the function cn_output.output_for_sheets.
+TODO: a function which calls them all, applies formulas and formats the excel sheet so as to reproduce the actual
+formatting.
 """
-
-import os
-import pkg_resources
-from ipp_macro_series_parser.config import Config
-
-from ipp_macro_series_parser.comptes_nationaux.cn_parser_main import cn_df_generator
-from ipp_macro_series_parser.comptes_nationaux.cn_extract_data import look_many
-from ipp_macro_series_parser.comptes_nationaux.cn_output import reshape_to_long_for_output, df_long_to_csv
-
-parser = Config(
-    config_files_directory = os.path.join(pkg_resources.get_distribution('ipp-macro-series-parser').location)
-    )
-cn_directory = parser.get('data', 'cn_directory')
-
-
-def output_for_sheets(entry_by_index_list, excel_file_name):
-    list_variables = entry_by_index_list
-    table = cn_df_generator(2013)
-
-    extract = look_many(table, list_variables)
-    extract = extract.drop_duplicates()
-    extract = extract.drop_duplicates((u'code', u'institution', u'ressources', u'value',
-       u'year'))  # this eliminates doubles, i.e. identical info coming from distinct sources (eg. TEE and Compte)
-
-    df = reshape_to_long_for_output(extract)
-
-    df_long_to_csv(df, excel_file_name)
-    return df
 
 
 # CN1
@@ -83,5 +59,28 @@ list_CN1 = [{'code': None, 'institution': 'S1', 'ressources': False, 'descriptio
         {'code': 'D39', 'institution': 'S13', 'ressources': True, 'description': ''}  # NB : D39 est aussi dans un autre compte (le compte d'exploitation) pour les APUs, en emplois. donc bien renseigner ressources..
     ]
 
+# CN2
 
-CN1 = output_for_sheets(list_CN1, os.path.join(cn_directory, u'Agrégats IPP - Comptabilité nationale.csv'))
+list_CN2 = [{'code': 'D11', 'institution': 'S11', 'ressources': False, 'description': ''},  # salaires versés. il nous les faut pour à peu près toutes les institutions
+        {'code': 'D11', 'institution': 'S12', 'ressources': False, 'description': ''},
+        {'code': 'D11', 'institution': 'S13', 'ressources': False, 'description': ''},
+        {'code': 'D11', 'institution': 'S14', 'ressources': False, 'description': ''},
+        {'code': 'D11', 'institution': 'S15', 'ressources': False, 'description': ''},
+        {'code': 'B2n', 'institution': 'S14', 'ressources': False, 'description': ''},  # ENE ménages (ie loyers reçus)
+        {'code': 'B3n', 'institution': 'S1', 'ressources': False, 'description': ''},   # Revenu mixte / non-salariés
+        {'code': 'D121', 'institution': 'S11', 'ressources': False, 'description': ''},  # cotisations patronales effectives
+        {'code': 'D121', 'institution': 'S12', 'ressources': False, 'description': ''},
+        {'code': 'D121', 'institution': 'S13', 'ressources': False, 'description': ''},
+        {'code': 'D121', 'institution': 'S14', 'ressources': False, 'description': ''},
+        {'code': 'D121', 'institution': 'S15', 'ressources': False, 'description': ''},
+        {'code': 'D122', 'institution': 'S11', 'ressources': False, 'description': ''},  # cotisations patronales imputées
+        {'code': 'D122', 'institution': 'S12', 'ressources': False, 'description': ''},
+        {'code': 'D122', 'institution': 'S13', 'ressources': False, 'description': ''},
+        {'code': 'D122', 'institution': 'S14', 'ressources': False, 'description': ''},
+        {'code': 'D122', 'institution': 'S15', 'ressources': False, 'description': ''},
+        {'code': 'B2n', 'institution': 'S11', 'ressources': False, 'description': ''},  # ENE SNF
+        {'code': 'B2n', 'institution': 'S12', 'ressources': False, 'description': ''},  # ENE SF
+        {'code': 'D21', 'institution': 'S13', 'ressources': True, 'description': ''},  # impôts indirects : impôts nets sur les produits & impôts nets sur la production
+        {'code': 'D31', 'institution': 'S13', 'ressources': True, 'description': ''},
+        {'code': 'D29', 'institution': 'S13', 'ressources': True, 'description': ''},
+        {'code': 'D39', 'institution': 'S13', 'ressources': True, 'description': ''}]
--- ipp_macro_series_parser/comptes_nationaux/cn_test.py
+++ ipp_macro_series_parser/comptes_nationaux/cn_test.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import os
+import pkg_resources
+from ipp_macro_series_parser.config import Config
 
 from ipp_macro_series_parser.comptes_nationaux.cn_parser_main import cn_df_generator
 from ipp_macro_series_parser.comptes_nationaux.cn_extract_data import look_many
 from ipp_macro_series_parser.comptes_nationaux import cn_output
+from ipp_macro_series_parser.comptes_nationaux import cn_sheets_lists
+
+parser = Config(
+    config_files_directory = os.path.join(pkg_resources.get_distribution('ipp-macro-series-parser').location)
+    )
+cn_directory = parser.get('data', 'cn_directory')
 
 
 folder_year = 2013
 entry_by_index_list = [{'code': None, 'institution': 'S1', 'ressources': False, 'description': 'PIB'},
              {'code': None, 'institution': 'S1', 'ressources': False, 'description': 'PIN'},
-            {'code': 'D121', 'institution': 'S11', 'ressources': False, 'description': None}  ]
-
-
+             {'code': 'D121', 'institution': 'S11', 'ressources': False, 'description': None}]
 df = cn_df_generator(folder_year)
 df1 = look_many(df, entry_by_index_list)
 df2 = cn_output.reshape_to_long_for_output(df1)
 cn_output.df_long_to_csv(df2, 'IPP_test.csv')
+
+
+CN1 = cn_output.output_for_sheets(
+    cn_sheets_lists.list_CN1, 2013,
+    os.path.join(cn_directory, u'Agrégats IPP - Comptabilité nationale.txt')
+    )
