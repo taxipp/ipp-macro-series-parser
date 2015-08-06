71fdbe350240602a83849755a5a340b74484ceaa
antoinearnoud <antoine.arnoud@gmail.com>
2015-08-06 16:28:51 +0200

    add df to hdf and hdf to df functions

--- ipp_macro_series_parser/comptes_nationaux/cn_parser_main.py
+++ ipp_macro_series_parser/comptes_nationaux/cn_parser_main.py
@@ -4,12 +4,20 @@ Created on Wed Jul 15 15:14:31 2015
 
 @author: Antoine
 """
-
+import os
 import pandas
+import pkg_resources
 
 
+from ipp_macro_series_parser.config import Config
 from ipp_macro_series_parser.comptes_nationaux.cn_parser_tee import tee_df_by_year_generator
 from ipp_macro_series_parser.comptes_nationaux.cn_parser_non_tee import non_tee_df_by_filename_generator
+
+parser = Config(
+    config_files_directory = os.path.join(pkg_resources.get_distribution('ipp-macro-series-parser').location)
+    )
+hdf_directory = parser.get('data', 'cn_hdf_directory')
+
 
 
 def cn_df_generator(year, list_years = None):
@@ -45,3 +53,16 @@ def cn_df_generator(year, list_years = None):
     df_full[['year']] = df_full[['year']].astype(int)
 
     return df_full
+
+
+def save_df_to_hdf(df, hdf_file_name, key):
+    file_path = os.path.join(hdf_directory, hdf_file_name)
+    df.to_hdf(file_path, key)
+    pandas.DataFrame().to_hdf
+
+
+def import_hdf_to_df(hdf_file_name, key):
+    file_path = os.path.join(hdf_directory, hdf_file_name)
+    store = pandas.HDFStore(file_path)
+    df = store[key]
+    return df
