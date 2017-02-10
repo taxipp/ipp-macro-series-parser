f047445b4730626939527329c78de7fb7b1333cc
simrab <simon.rabate@ipp.eu>
2017-02-10 16:18:06 +0100

    TO DO: adapte prelevements_sociaux to prestations sociales

---
+++ ipp_macro_series_parser/scripts/prelevements_sociaux_parser.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Parse dénombrements fiscaux to produce the dataframe stroed in a HDF5 file
+"""
+
+
+import argparse
+import logging
+import os
+import sys
+
+
+from ipp_macro_series_parser.config import Config
+from ipp_macro_series_parser.prelevements_sociaux import main
+app_name = os.path.splitext(os.path.basename(__file__))[0]
+log = logging.getLogger(app_name)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('-e', '--end', default = 2013, help = 'ending year to be downloaded')
+    # parser.add_argument('-s', '--start', default = 2009, help = 'starting year to be downloaded')
+    parser.add_argument('-v', '--verbose', action = 'store_true', default = False, help = "increase output verbosity")
+    parser.add_argument('-f', '--force', action = 'store_true', default = False,
+        help = "Force overwrite of existing data")
+
+    args = parser.parse_args()
+    logging.basicConfig(level = logging.DEBUG if args.verbose else logging.WARNING, stream = sys.stdout)
+
+    config = Config()
+    directory = config.get('data', 'prestations_sociales_directory')
+    assert directory != 'None', \
+        "Set prestations_sociales_directory in the data section of you config[_local].ini file to a valid directory"
+
+    # years = range(args.start, args.end + 1)
+    hdf_file_path = os.path.join(directory, 'prestations_sociales.h5')
+    if os.path.exists(hdf_file_path):
+        if not args.force:
+            log.error("The file {} already exists. Use the --force to overwrite.".format(hdf_file_path))
+            return
+
+    create_prestations_sociales_data_frames()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
