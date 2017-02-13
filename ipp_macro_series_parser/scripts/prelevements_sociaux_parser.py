354bb887f4b0d0df1dafa69a56d8bc062cc84e4f
simrab <simon.rabate@ipp.eu>
2017-02-13 11:52:15 +0100

    Modif parsing prelevement sociaux

--- ipp_macro_series_parser/scripts/prelevements_sociaux_parser.py
+++ ipp_macro_series_parser/scripts/prelevements_sociaux_parser.py
@@ -13,15 +13,15 @@ import sys
 
 
 from ipp_macro_series_parser.config import Config
-from ipp_macro_series_parser.prelevements_sociaux import main
+from ipp_macro_series_parser.prelevements_sociaux.parser import main_parse
+
 app_name = os.path.splitext(os.path.basename(__file__))[0]
 log = logging.getLogger(app_name)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    # parser.add_argument('-e', '--end', default = 2013, help = 'ending year to be downloaded')
-    # parser.add_argument('-s', '--start', default = 2009, help = 'starting year to be downloaded')
+
     parser.add_argument('-v', '--verbose', action = 'store_true', default = False, help = "increase output verbosity")
     parser.add_argument('-f', '--force', action = 'store_true', default = False,
         help = "Force overwrite of existing data")
@@ -30,18 +30,26 @@ def main():
     logging.basicConfig(level = logging.DEBUG if args.verbose else logging.WARNING, stream = sys.stdout)
 
     config = Config()
-    directory = config.get('data', 'prestations_sociales_directory')
-    assert directory != 'None', \
-        "Set prestations_sociales_directory in the data section of you config[_local].ini file to a valid directory"
+    prelevements_sociaux_source = config.get('data', 'prelevements_sociaux_source')
+    prelevements_sociaux_directory = config.get('data', 'prelevements_sociaux_directory')
+    assert prelevements_sociaux_source != 'None', \
+    "Set prelevements_sociaux_source in the data section of you config[_local].ini file to a valid directory"
+    assert prelevements_sociaux_directory != 'None', \
+    "Set prelevements_sociaux_directory in the data section of you config[_local].ini file to a valid directory"
 
-    # years = range(args.start, args.end + 1)
-    hdf_file_path = os.path.join(directory, 'prestations_sociales.h5')
-    if os.path.exists(hdf_file_path):
+    clean_directory = os.path.join(prelevements_sociaux_directory,'clean')
+    if not os.path.exists(clean_directory):
+        os.makedirs(clean_directory)
+
+    file_path1 = os.path.join(clean_directory, 'recette_csg_crds.csv')
+    file_path2 = os.path.join(clean_directory, 'recette_csg_by_type.csv')
+
+    if os.path.exists(file_path1) or  os.path.exists(file_path2):
         if not args.force:
-            log.error("The file {} already exists. Use the --force to overwrite.".format(hdf_file_path))
+            log.error("The files {} and/or {} already exist. Use the --force to overwrite.".format(file_path1, file_path2))
             return
 
-    create_prestations_sociales_data_frames()
+    main_parse()
 
 
 if __name__ == "__main__":
